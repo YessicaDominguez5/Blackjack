480bcf2b67afc239d739851dedd2061a268e75dc
YessicaDominguez5 <yessidom5@gmail.com>
2025-01-02 19:05:51 -0300

    pedir carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -7,6 +7,8 @@ let deck = [];
 const tipos = ['C', 'H', 'D', 'S'];
 const especiales = ['A', 'J', 'Q', 'K'];
 
+//Crea una nueva baraja 
+
 const crearDeck = () => {
 
 for(let i = 2; i <=10; i++){
@@ -27,9 +29,8 @@ for(let tipo of tipos){
 
 }
 
-console.log(deck);
 
-deck = _.shuffle(deck);
+deck = _.shuffle(deck); //función de la librería undescore. Cambia de posición los elementos del array de manera aleatoria.
 console.log(deck);
 
 
@@ -37,3 +38,38 @@ console.log(deck);
 }
 
 crearDeck();
+
+//Pedir una carta del mazo(lógica del mazo)
+
+const pedirCarta = ()=>{
+
+
+if(deck.length === 0){
+throw 'No hay cartas en el deck';
+
+}
+
+let carta = deck.pop();
+//console.log(carta);
+
+//console.log(deck);
+
+return carta;
+}
+
+//pedirCarta();
+
+//Tomar el valor de la carta
+
+const valorCarta = (carta) =>{
+
+    const valor = carta.substring(0, carta.length - 1); //El substring divide el string, en este
+    //caso le indicamos que es desde la posición 0 a la anteúltima, es decir le borramos el palo 
+    //ej 2C quedaría 2, 10H quedaría 10.
+
+    return isNaN(valor)?((valor === 'A')?11:10):valor*1;
+
+
+}
+
+const valor = valorCarta(pedirCarta());
